108ffa8880ea889d8a4dd9c626e0fb4687a97f59
Humberto Tello <betoelo2@gmail.com>
2020-06-09 15:27:24 -0300

    new layout

--- src/components/Subjects/Subjects.jsx
+++ src/components/Subjects/Subjects.jsx
@@ -1,40 +1,46 @@
 import React from 'react';
 import { Layout, Button, Typography } from 'antd';
-import './Subjects.css'
-import { RightOutlined } from '@ant-design/icons';
+import './Subjects.scss'
+import MobileHeader from '../MobileHeader';
+import BrowserHeader from '../BrowserHeader';
 
-const { Text, Title } = Typography;
-const { Header, Footer, Content } = Layout;
+const { Text } = Typography;
+const { Footer, Content } = Layout;
 
 const Subjects = () => (
     <div>
-        <Layout className='layoutSubject' style={{background: '#0A6777', align: 'center'}}>
+        <Layout className='layoutSubject' style={{background: 'white', align: 'center'}}>
             
-            <Header className='header' style={{background: '#0A6777', color: '#FFFFFF', fontSize: '25px'}}>
-                Disciplinas   
-            </Header>
+            <MobileHeader title="Disciplinas" />
+            <BrowserHeader title="Disciplinas" />
 
             <Content className='content' style={{flex: 1}}>
                 <div className='button' style={{width: 330, margin: 'auto', height: 1000}}>
-                    <Button style = {{background: 'rgba(150, 159, 170, 0.38)', color: 'white', marginBottom: '7px', border: '1px'}} /*icon={<RightOutlined />*/>
-                        <Text strong style={{color: 'white'}}>Disciplina 1</Text>
+                    <Button style = {{background: 'rgba(150, 159, 170, 0.38)', color: 'white', marginBottom: '7px', border: '1px'}}>
+                        <Text strong style={{color: 'black'}}>Disciplina 1</Text>
+                        <i className="icon fas fa-chevron-right" />
                     </Button>
-                    <Button style = {{background: 'rgba(150, 159, 170, 0.38)', color: 'white', marginBottom: '7px', border: '1px'}} /*icon={<RightOutlined />*/>
-                        <Text strong style={{color: 'white'}}>Disciplina 2</Text>
+                    <Button style = {{background: 'rgba(150, 159, 170, 0.38)', color: 'black', marginBottom: '7px', border: '1px'}}>
+                        <Text strong style={{color: 'black'}}>Disciplina 2</Text>
+                        <i className="icon fas fa-chevron-right" />
                     </Button>
-                    <Button style = {{background: 'rgba(150, 159, 170, 0.38)', color: 'white', marginBottom: '7px', border: '1px'}} /*icon={<RightOutlined />*/>
-                        <Text strong style={{color: 'white'}}>Disciplina 3</Text>
+                    <Button style = {{background: 'rgba(150, 159, 170, 0.38)', color: 'black', marginBottom: '7px', border: '1px'}}>
+                        <Text strong style={{color: 'black'}}>Disciplina 3</Text>
+                        <i className="icon fas fa-chevron-right" />
                     </Button>
-                    <Button style = {{background: 'rgba(150, 159, 170, 0.38)', color: 'white', marginBottom: '7px', border: '1px'}} /*icon={<RightOutlined />*/>
-                        <Text strong style={{color: 'white'}}>Disciplina 4</Text>
+                    <Button style = {{background: 'rgba(150, 159, 170, 0.38)', color: 'black', marginBottom: '7px', border: '1px'}}>
+                        <Text strong style={{color: 'black'}}>Disciplina 4</Text>
+                        <i className="icon fas fa-chevron-right" />
                     </Button>
-                    <Button style = {{background: 'rgba(150, 159, 170, 0.38)', color: 'white', marginBottom: '7px', border: '1px'}} /*icon={<RightOutlined />*/>
-                        <Text strong style={{color: 'white'}}>Disciplina 5</Text>
+                    <Button style = {{background: 'rgba(150, 159, 170, 0.38)', color: 'black', marginBottom: '7px', border: '1px'}}>
+                        <Text strong style={{color: 'black'}}>Disciplina 5</Text>
+                        <i className="icon fas fa-chevron-right" />
                     </Button>
-                    <Button style = {{background: 'rgba(150, 159, 170, 0.38)', color: 'white', marginBottom: '7px', border: '1px'}} /*icon={<RightOutlined />*/>
-                        <Text strong style={{color: 'white'}}>Disciplina 6</Text>
+                    <Button style = {{background: 'rgba(150, 159, 170, 0.38)', color: 'black', marginBottom: '7px', border: '1px'}}>
+                        <Text strong style={{color: 'black'}}>Disciplina 6</Text>
+                        <i className="icon fas fa-chevron-right" />
                     </Button>
-                    <Button style = {{background: '#1094AB', color: 'white', marginTop: '15px', border: '1px'}}>
+                    <Button style = {{background: '#1094ab', color: '#1094ab', marginTop: '15px', border: '1px'}}>
                         <Text strong style={{color: 'white'}}>Criar disciplina</Text>
                     </Button>
                 </div>
