420b147656c3a3d6303dc353ca4510c397e3a06e
bianca vieira <bncvr@outlook.com>
2020-06-14 23:09:58 -0300

    Remove unnecessary code

--- src/components/Input/TextField.jsx
+++ src/components/Input/TextField.jsx
@@ -4,13 +4,12 @@ import classNames from 'classnames';
 import Label from './Label';
 import './TextField.scss';
 
-const TextField = ({ name, label, onChange = () => {}, color, required, error }) => (
-  <div key="label" className={classNames('text-field-container', name, { 'error': error })}>
+const TextField = ({ name, label, onChange = () => { }, color, required }) => (
+  <div key="label" className={classNames('text-field-container', name)}>
     <div className={classNames('input-container', { [color]: color })}>
       <Label name={label} required={required} />
       <Input onChange={onChange} name={name} />
     </div>
-    {error && (<span className="error-msg">{error}</span>)}
   </div>
 );
 
