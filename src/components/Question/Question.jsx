76a4b02d293eeabcac3229898ef21d4edecb417e
bianca vieira <bncvr@outlook.com>
2020-07-01 19:46:29 -0300

    Handle selected and style changes

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,45 +1,56 @@
 import React from 'react';
 import { Layout, Typography, Row, Col } from 'antd';
+import classNames from 'classnames';
 import './Question.scss'
 import MobileHeader from '../MobileHeader';
 import BrowserHeader from '../BrowserHeader';
-import BottomButton from '../BottomButton';
 
 const { Text } = Typography;
 
-const defaultQuestionNames = [
-  { name: 'Lorem ipsum dolor sit amet consectetur adipiscing elit erat porta elementum' },
-  { name: 'Lorem ipsum dolor sit amet consectetur adipiscing elit erat porta elementum' },
-  { name: 'Lorem ipsum dolor sit amet consectetur adipiscing elit erat porta elementum' },
-  { name: 'Lorem ipsum dolor sit amet consectetur adipiscing elit erat porta elementum' },
-  { name: 'Lorem ipsum dolor sit amet consectetur adipiscing elit erat porta elementum' },
-];
+const defaultQuestion = {
+  id: 1,
+  name: 'Lorem ipsum dolor sit amet consectetur adipiscing elit erat porta elementum?',
+  answers: [
+    { text: 'Texto alternativa 1', correct: false },
+    { text: 'Texto alternativa 2', correct: false },
+    { text: 'Texto alternativa 3', correct: false },
+    { text: 'Texto alternativa 4', correct: false },
+    { text: 'Texto alternativa 5', correct: false },
+  ]
+};
 
-const Question = ({ questionNames = defaultQuestionNames }) => (
-  <Layout className='layoutQuestion'>
-    <MobileHeader title="Pergunta 1" color="white" />
-    <BrowserHeader title="Pergunta 1" />
-    <Row>
-      <Col sm={{ span: 24 }} md={{ span: 18 }} lg={{ span: 12 }}>
-        <div className="question-title">
-          <p>Quanto é 2 + 2?</p>
-        </div>
-        {questionNames.map((question, index) => (
-          <div key={index} className="box">
-            <div key={index} className='button'>
-              <Text className="letter-text">{String.fromCharCode(index + 65)}</Text>
-              <Text className="question-name">{question.name}</Text>
-            </div>
+const Question = ({ question = defaultQuestion }) => {
+  const [selected, setSelected] = React.useState();
+
+  return (
+    <Layout className='layoutQuestion'>
+      <MobileHeader title="Pergunta 1" color="black" />
+      <BrowserHeader title={`Pergunta ${question.id}`} />
+      <Row>
+        <Col sm={{ span: 24 }} md={{ span: 18 }} lg={{ span: 12 }}>
+          <div className="question-title">
+            <p>{question.name}</p>
           </div>
-        ))}
-      </Col>
-    </Row>
-    <div className="bottom-buttons">
-      <button>Anterior</button>
-      <button>Próximo</button>
-    </div>
-
-  </Layout>
-);
+          {question.answers.map((answer, index) => (
+            <div key={index} className="box">
+              <div
+                key={index}
+                className={classNames('answer-container', { 'selected': selected === index })}
+                onClick={() => setSelected(index)}
+              >
+                <Text className="letter-text">{String.fromCharCode(index + 65)}</Text>
+                <Text className="answer-name">{answer.text}</Text>
+              </div>
+            </div>
+          ))}
+        </Col>
+      </Row>
+      <div className="bottom-buttons">
+        <button><span>Anterior</span></button>
+        <button><span>Próximo</span></button>
+      </div>
+    </Layout>
+  )
+};
 
 export default Question;
